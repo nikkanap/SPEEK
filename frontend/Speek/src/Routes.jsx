75c3c7d43a96c2a26fe26786ce69b9aeb4a65172
nikkanap <nenaputo@up.edu.ph>
2026-07-14 14:24:01 +0800

    Create new Routes.jsx and Replace App with AppRoutes in main.jsx

---
+++ frontend/Speek/src/Routes.jsx
@@ -0,0 +1,28 @@
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/auth/Login";
+import Signup from "./pages/auth/Signup"; 
+
+// router for creating the browser routes
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <> 
+      <Route path="/" element={<Home />} /> 
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </>
+  )
+)
+
+
+// Where the app is
+const AppRoutes = () => {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default AppRoutes
