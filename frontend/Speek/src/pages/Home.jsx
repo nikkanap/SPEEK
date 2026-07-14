00b1158f74dcdafac8f5f6abcbfa09be4ae81b81
nikkanap <nenaputo@up.edu.ph>
2026-07-14 14:25:24 +0800

    Create home jsx and css files

---
+++ frontend/Speek/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from './assets/vite.svg'
+//import heroImg from './assets/hero.png'
+import './Home.css'
+
+function Home() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      Hello World
+    </>
+  )
+}
+
+export default Home
