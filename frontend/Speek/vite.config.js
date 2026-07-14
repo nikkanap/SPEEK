55e895f6320ba05bb6418b8bd87925705d2c8116
nikkanap <nenaputo@up.edu.ph>
2026-07-14 15:59:14 +0800

    Update vite.config.js file with updated ports

--- frontend/Speek/vite.config.js
+++ frontend/Speek/vite.config.js
@@ -4,8 +4,19 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080"
+  }
 })
