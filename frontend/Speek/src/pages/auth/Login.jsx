31ad222b4bf95e14d5e70b9fd409453cc48c3e55
nikkanap <nenaputo@up.edu.ph>
2026-07-14 15:05:47 +0800

    Add general styling to Login and Signup jsx files using shared Forms.css

--- frontend/Speek/src/pages/auth/Login.jsx
+++ frontend/Speek/src/pages/auth/Login.jsx
@@ -1,20 +1,36 @@
+import Header from '../../components/Header'
 import './Login.css'
+import './Forms.css'
 
 function Login() {
   //const [count, setCount] = useState(0)
+  const handleLogin = () => {
+    alert('Pressed login button')
+  }
 
   return (
     <>
-      <p>Login Page</p>
-      <form>
+    <Header />
+      <h1>Login Page</h1>
+      <form action={handleLogin} className='form-container'>
         <label>
           Username
-          <input type='text' />
+          <input 
+            type='text'
+            placeholder='Enter username'
+          />
         </label>
         <label>
           Password
-          <input type='password' />
+          <input 
+            type='password' 
+            placeholder='Enter password'
+          />
         </label>
+        <div className='form-buttons'>
+          <button type='submit'>Login</button>
+          <button type='reset'>Clear Form</button>
+        </div>
       </form>
     </>
   )
--- frontend/Speek/src/pages/auth/Signup.jsx
+++ frontend/Speek/src/pages/auth/Signup.jsx
@@ -1,28 +1,50 @@
+import Header from '../../components/Header'
 import './Signup.css'
 
 function Signup() {
   //const [count, setCount] = useState(0)
 
+  const handleSignup = () => {
+    alert("Pressed signup button")
+  }
+
   return (
     <>
+      <Header />
       <p>Signup Page</p>
-      <form>
+      <form action={handleSignup} className='form-container'>
         <label>
           Username
-          <input type='text' />
+          <input 
+            type='text' 
+            placeholder='Create a username'
+          />
         </label>
         <label>
-          Emaill Address
-          <input type='text' />
+          Email Address
+          <input 
+            type='text' 
+            placeholder='Add email address'
+          />
         </label>
         <label>
           Password
-          <input type='password' />
+          <input 
+            type='password' 
+            placeholder='Create password'
+          />
         </label>
         <label>
           Confirm Password
-          <input type='password' />
+          <input 
+            type='password' 
+            placeholder='Confirm password'
+          />
         </label>
+        <div className='form-buttons'>
+          <button type='submit'>Signup</button>
+          <button type='reset'>Clear Form</button>
+        </div>
       </form>
     </>
   )
