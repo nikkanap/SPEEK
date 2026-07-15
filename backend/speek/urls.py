92fe53c9f70b6538cc9e11d0685845ab952429f0
nikkanap <nenaputo@up.edu.ph>
2026-07-15 15:34:59 +0800

    Fix url leading to webserver.urls

--- backend/speek/urls.py
+++ backend/speek/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import backend.webserver as webserver
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', webserver.urls)
+    path('api/', include('webserver.urls'))
 ]
