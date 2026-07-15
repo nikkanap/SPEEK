adccaa0d8b57b2a846d0d1ff4f63721f72b2c230
nikkanap <nenaputo@up.edu.ph>
2026-07-15 15:24:50 +0800

    Add load_dotenv() to load the env file

--- backend/speek/settings.py
+++ backend/speek/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from os import getenv
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4yc^==0+t^@u32nyl-8mvh74*ffl(#p5xyhq0j=iv0fs01s9sc'
+SECRET_KEY = getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
